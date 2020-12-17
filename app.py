3c5bbbc00393dc1b754b23c6fa93c69271e1eecd
dhtdht020 <dhtdht020@gmail.com>
2020-12-17 15:07:07 +0200

    Added error page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-import metadata
+import osc
 
-metadata = metadata.API()
-metadata.load_packages()
+OpenShopChannel = osc.API()
+OpenShopChannel.load_packages()
 
 app = Flask(__name__)
 
@@ -11,19 +11,39 @@ app = Flask(__name__)
 def welcome():
     return render_template('welcome.html')
 
+
 @app.route("/home")
 def home():
-    return render_template('home.html', packages=metadata.get_packages())
+    return render_template('home.html', packages=OpenShopChannel.get_packages())
 
 
 @app.route("/apps")
 def apps():
-    return render_template('list.html', packages=metadata.get_packages())
+    return render_template('list.html', packages=OpenShopChannel.get_packages())
 
 
 @app.route("/app")
 def application():
-    return render_template('app.html', package=metadata.package_by_name(request.args.get("package")))
+    package = OpenShopChannel.package_by_name(request.args.get("package"))
+    if package:
+        return render_template('app.html', package=package)
+    else:
+        return render_template('error.html', code="APP-404",
+                               description="The app \"{}\" (Internal Name) could not be found.".format(
+                                   request.args.get("package")))
+
+
+# Errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', code="HTTP-404",
+                           description="Page could not be found. This is not where the shop is :(")
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error.html', code="HTTP-500",
+                           description="Something went wrong on the shop. Please report this event!")
 
 
 if __name__ == '__main__':
