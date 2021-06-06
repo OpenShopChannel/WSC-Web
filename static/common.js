d40b6b8b28dc39cb7ed619fdacd6ca18db2eb1d6
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-06 19:59:26 -0400

    Put all JS into common.js

--- static/common.js
+++ static/common.js
@@ -1,2 +1,20 @@
 var shop = new wiiShop();
+var sound = new wiiSound();
+var keypad = new wiiKeyboard();
 const unused = shop.connecting;
+
+// Shop
+
+
+// Sounds
+var bgm = sound.playBGM();
+
+function playSE(num) {
+    sound.playSE(num);
+}
+
+// Keyboard
+function keyboard(type, rowLimit, isPassword, title) {
+    // example: keyboard(0, 10, false, "title")
+    var keyboard = keypad.call(type, rowLimit, isPassword, title);
+}
