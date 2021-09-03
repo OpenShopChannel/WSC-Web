d4f83d3adc02c44839063b41fd011162ea508233
ramblecube <andrew7d2@gmail.com>
2021-09-02 21:25:19 -0700

    Add human-readable filesize formatting to osc.py
    Adds the method `sizeof_fmt` to osc.py, which reformats a given value in bytes to the nearest human-readable byte unit size.
    
    E.g., 5000 bytes would become 5kb.
    
    Changes the "extracted" value of packages returned by `package_by_name` and `search_packages` to a human-readable format using the aforementioned method.

--- osc.py
+++ osc.py
@@ -5,6 +5,12 @@ from functools import reduce
 import requests
 import operator
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 def build_dictionary_filter_from_kwargs(**filter_kwargs):
     def _filter_fun(field, value):
@@ -39,6 +45,7 @@ class API:
             if package["internal_name"] == name:
                 try:
                     package["release_date"] = datetime.fromtimestamp(int(package["release_date"])).strftime('%B %e, %Y')
+                    package["extracted"] = sizeof_fmt(package["extracted"])
                 except ValueError:
                     pass
                 return package
@@ -51,6 +58,7 @@ class API:
             if value.lower() in package[key].lower():
                 try:
                     package["release_date"] = datetime.fromtimestamp(int(package["release_date"])).strftime('%B %e, %Y')
+                    package["extracted"] = sizeof_fmt(package["extracted"])
                 except ValueError:
                     pass
                 matching_packages.append(package)
