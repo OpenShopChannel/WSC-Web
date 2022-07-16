8db6b427a79abcfbc58b55478ac368de2c1c3f7b
Spotlight <spotlight@joscomputing.space>
2022-07-15 21:57:27 -0500

    Adjust download formatting

--- static/js/download.js
+++ static/js/download.js
@@ -1,21 +1,22 @@
 /**
  * The title ID of our download manager channel ("OSCD").
  */
- var downloaderChannelID = "0x000100084f534344";
- var downloaderChannelTicketID = "0x0001000000000000";
+var downloaderChannelID = "0x000100084f534344";
+var downloaderChannelTicketID = "0x0001000000000000";
+var downloaderChannelVersion = 1;
  
  /**
   * Whether an existing download operation is ongoing.
   */
- var startingDownload = false;
+var startingDownload = false;
  
  /**
   * Begins downloading for the passed title ID.
   * 
   * @param {string} titleId The title ID to begin downloading  
   */
- function startDownload(titleId) {
-     if (!startingDownload) {
+function startDownload(titleId) {
+    if (!startingDownload) {
         startingDownload = true;
 
         // TODO: ensure download manager is installed
@@ -25,6 +26,5 @@
         setTimeout(function(){
             ec.launchTitle(downloaderChannelID, downloaderChannelTicketID);
         }, 2000);
-     }
- } 
- 
+ }
+
